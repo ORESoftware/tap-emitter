ac5d97a70da0e48c434ba81386203901a0eefbe1
James Talmage <james@talmage.io>
2016-03-20 01:06:18 -0400

    add fixture test: child-extra

--- test/fixtures.js
+++ test/fixtures.js
@@ -19,34 +19,43 @@ function tryLoad(...args) {
 	throw new Error('found none of these: ' + args.join(', '));
 }
 
+function processEvents(events, lines, indent, opts) {
+	const ee = new EventEmitter();
+	const messages = new Messages(13, indent);
+
+	ee.on('version', () => lines.push(messages.version()));
+	ee.on('plan', plan => lines.push(messages.plan(plan.end)));
+	ee.on('bailout', message => lines.push(messages.bailout(message)));
+	ee.on('comment', comment => lines.push(messages.diagnostic(comment.replace(/\n$/, '').replace(/^\s*#\s+/, ''))));
+	ee.on('child', events => processEvents(events, lines, indent + 4, opts));
+	ee.on('assert', assert => {
+		const def = {
+			ok: assert.ok,
+			description: assert.name
+		};
+		if (opts.id !== false) {
+			def.testNumber = assert.id;
+		}
+		if (assert.time) {
+			def.directive = 'time=' + assert.time + 'ms';
+		}
+		lines.push(messages.test(def));
+		if (assert.diag) {
+			lines.push(messages.yaml(assert.diag));
+		}
+	});
+
+	atoe(ee, events);
+}
+
 function runTest(name, opts) {
 	opts = opts || {};
 
 	(opts.debug ? test.only : test)(name, t => {
 		const events = JSON.parse(loadFixture(name + '.json'));
 		const expected = tryLoad(name + '.tap-out', name + '.tap').trim();
-		const ee = new EventEmitter();
 		const lines = [];
-		const messages = new Messages(13, 0);
-
-		ee.on('version', () => lines.push(messages.version()));
-		ee.on('plan', plan => lines.push(messages.plan(plan.end)));
-		ee.on('bailout', message => lines.push(messages.bailout(message)));
-		ee.on('assert', assert => {
-			const def = {
-				ok: assert.ok,
-				description: assert.name
-			};
-			if (opts.id !== false) {
-				def.testNumber = assert.id;
-			}
-			lines.push(messages.test(def));
-			if (assert.diag) {
-				lines.push(messages.yaml(assert.diag));
-			}
-		});
-
-		atoe(ee, events);
+		processEvents(events, lines, 0, opts);
 
 		const output = lines.join('\n').trim();
 
@@ -63,5 +72,6 @@ runTest('basic', {id: false});
 runTest('bignum');
 runTest('bignum_many');
 runTest('broken-yamlish-looks-like-child');
+runTest('child-extra');
 
 runTest('simple_yaml');
